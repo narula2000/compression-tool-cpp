c92af2619cbbd60074ca6ba2d3cadadb301e8e2f
Vikrom Narula <vikrom.nar@gmail.com>
2026-09-09 11:41:18 +0700

    feat: build huffman graph

--- src/main.cpp
+++ src/main.cpp
@@ -32,12 +32,13 @@ std::map<char, int> build_counter_map(std::ifstream &file) {
 
 struct Node {
   char character;
-  int frequency;
+  int frequency = 0;
   Node *left = nullptr;
   Node *right = nullptr;
 
   Node(char character, int frequency)
       : character(character), frequency(frequency) {}
+  Node(int frequency) : frequency(frequency) {}
 };
 
 struct MinHeapCompare {
@@ -47,14 +48,39 @@ struct MinHeapCompare {
 };
 
 std::priority_queue<Node *, std::vector<Node *>, MinHeapCompare>
-build_graph(std::map<char, int> &counter) {
+build_queue(std::map<char, int> &counter) {
   std::priority_queue<Node *, std::vector<Node *>, MinHeapCompare> queue;
 
   for (auto item : counter) {
     queue.push(new Node(item.first, item.second));
   }
-
   return queue;
+}
+
+Node *build_graph(
+    std::priority_queue<Node *, std::vector<Node *>, MinHeapCompare> &queue) {
+
+  Node *left;
+  Node *right;
+  int frequency;
+  Node *head;
+  while (queue.size() > 1) {
+    left = queue.top();
+    queue.pop();
+    right = queue.top();
+    queue.pop();
+    frequency = left->frequency + right->frequency;
+
+    Node *dummy = new Node(frequency);
+    dummy->left = left;
+    dummy->right = right;
+    queue.push(dummy);
+  }
+
+  head = queue.top();
+  queue.pop();
+
+  return head;
 }
 
 int main(int argc, char *argv[]) {
@@ -110,6 +136,7 @@ int main(int argc, char *argv[]) {
   }
   std::map<char, int> counter;
   std::priority_queue<Node *, std::vector<Node *>, MinHeapCompare> queue;
+  Node *head;
   if (mode == "encode") {
     if (input_path.empty() or bin_path.empty()) {
       std::cout << "Please provide a file input and a binary output path\n";
@@ -127,13 +154,8 @@ int main(int argc, char *argv[]) {
 
     counter = build_counter_map(input_file);
 
-    queue = build_graph(counter);
-    Node *node;
-    while (!queue.empty()) {
-      node = queue.top();
-      queue.pop();
-      std::cout << node->character << " : " << node->frequency << "\n";
-    }
+    queue = build_queue(counter);
+    head = build_graph(queue);
   } else if (mode == "decode") {
     if (output_path.empty() or bin_path.empty()) {
       std::cout
